3a68176ffbc3d88c59556124891e41c797630eda
Ian Torres <iantorres@outlook.com>
2019-05-26 20:06:09 -0400

    Autenticación añadida.

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+
+class AuthTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_redirect_if_authenticated()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)
+            ->get('/login');
+        $response->assertRedirect('home');
+    }
+
+    public function test_reset_password_route()
+    {
+        $response = $this->get('/password/reset');
+        $response->assertStatus(200);
+    }
+
+    public function test_register_route()
+    {
+        $user = factory(User::class)->make();
+        $password = substr(md5(mt_rand()), 0, 6);
+        $response = $this->post('/register', [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => $password,
+            'password_confirmation' => $password,
+        ]);
+        $response->assertRedirect('home');
+    }
+
+    public function test_home_route()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)
+            ->get('/home');
+        $response->assertStatus(200);
+    }
+
+    public function test_reset_password_constructor()
+    {
+        $user = factory(User::class)->make();
+        $response = $this->post('/password/reset', [
+            'email' => $user->email,
+        ]);
+        $response->assertStatus(302);
+    }
+}
