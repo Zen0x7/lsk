daea659c0d23296f3f24298263a4132ec071019d
Ian Torres <iantorres@outlook.com>
2019-06-14 20:04:30 -0500

    Método guardar añadido.

--- app/Http/Controllers/SettingsOrganizationsController.php
+++ app/Http/Controllers/SettingsOrganizationsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Organization;
 use Illuminate\Http\Request;
 
 class SettingsOrganizationsController extends Controller
@@ -9,5 +10,13 @@ class SettingsOrganizationsController extends Controller
     public function index()
     {
         return view('settings.organizations.index');
+    }
+
+    public function store(Request $request)
+    {
+        $organization = $request->user()->organizations()->create($request->only([
+            'name'
+        ]));
+        return response($organization, 201);
     }
 }
